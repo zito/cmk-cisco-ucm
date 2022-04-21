d839024e4c2c025527aad55351ccce1dddcca8f3
Václav Ovsík <vaclav.ovsik@gmail.com>
2022-04-21 14:32:09 +0200

    version 1.2

--- agent_based/cisco_ucm_services.py
+++ agent_based/cisco_ucm_services.py
@@ -4,61 +4,210 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Generator, List, Mapping, NamedTuple, Optional
 from .agent_based_api.v1.type_defs import CheckResult, StringTable, DiscoveryResult
-from .agent_based_api.v1 import register, Result, Service, State
+from .agent_based_api.v1 import regex, register, Result, Service, State
 
 
-Section = Dict[str, Any]
+CISCO_UCM_SERVICES_DISCOVERY_DEFAULT_PARAMETERS: Dict[str, Any] = {
+    "state": "Started",
+}
 
-def parse_cisco_ucm_services(string_table: StringTable) -> Optional[Section]:
-    if not string_table:
-        return None
+CISCO_UCM_SERVICES_CHECK_DEFAULT_PARAMETERS = {
+    "states": [("Started", 0)],
+    "else": 2,
+    "additional_servicenames": [],
+}
 
-    by_state: Dict = {}
-    all_services_ok = True
-    svc_state = []
-    for name, status, reason_code, reason_str in string_table:
-        by_state.setdefault(status, 0)
-        by_state[status] += 1
-        s = f"{name}: {status}"
-        if reason_str:
-            s += " (" + reason_str + ")"
-        if status != 'Started' and reason_str != 'Service Not Activated':
-            svc_state.append( (State.CRIT, s) )
-            all_services_ok = False
-        else:
-            svc_state.append( (State.OK, s) )
+CISCO_UCM_SERVICES_SUMMARY_DEFAULT_PARAMETERS = {"ignored": [], "state_if_stopped": 0}
 
-    section: Section = {
-        'num_started_services': by_state.get('Started', 0),
-        'all_services_ok': all_services_ok,
-        'service_state': svc_state,
-    }
 
-    return section
+class CUCMService(NamedTuple):
+    name: str
+    state: str
+    reason_code: str
+    reason_str: str
 
-def discover_cisco_ucm_services(section: Section) -> DiscoveryResult:
-    if section['num_started_services']:
-        yield Service()
 
-def check_cisco_ucm_services(section: Section) -> CheckResult:
-    if section['all_services_ok']:
-        yield Result(state = State.OK, summary = 'All services are OK')
-    else:
-        yield Result(state = State.CRIT, summary = 'Some services are in bad state')
-    for state, s in section['service_state']:
-        yield Result(state = state, notice = s)
+Section = List[CUCMService]
 
+
+def parse_cisco_ucm_services(string_table: StringTable) -> Section:
+    return [
+        CUCMService(name, state, reason_code, reason_str)
+        for name, state, reason_code, reason_str in string_table
+    ]
 
 register.agent_section(
     name = "cisco_ucm_services",
     parse_function = parse_cisco_ucm_services,
 )
 
+
+def discovery_cisco_ucm_services(params: List[Dict[str, Any]], section: Section) -> DiscoveryResult:
+    # Handle single entries (type str)
+    def add_matching_services(service: CUCMService, entry):
+        # New wato rule handling
+        svc, state = entry
+        # First match name (optional since rule based config option available)
+        if svc:
+            if not svc.startswith("~") and svc != service.name:
+                return
+
+            r = regex(svc[1:])
+            if not r.match(service.name):
+                return
+
+        if state and state.lower() != service.state.lower():
+            return
+
+        yield Service(item=service.name)
+
+    # Extract the WATO compatible rules for the current host
+    rules = []
+
+    for value in params:
+        # Now extract the list of service regexes
+        svcs = value.get('cisco_ucm_services', [])
+        service_state = value.get('state', None)
+        if svcs:
+            for svc in svcs:
+                rules.append(('~' + svc, service_state))
+        else:
+            rules.append((None, service_state))
+
+    for service in section:
+        for rule in rules:
+            yield from add_matching_services(service, rule)
+
+
+def check_cisco_ucm_services_single(
+    item: str,
+    params: Mapping[str, Any],
+    section: Section,
+) -> Generator[Result, None, None]:
+    additional_names = params.get("additional_servicenames", [])
+    for service in section:
+        if (item == service.name) or service.name in additional_names:
+            summary = f"{service.name}: {service.state}"
+            if int(service.reason_code) > 0:
+                summary += f" {service.reason_code}: {service.reason_str}"
+            yield Result(
+                state=_match_service_against_params(params, service),
+                summary=summary,
+            )
+
+
+def _match_service_against_params(params: Mapping[str, Any], service: CUCMService) -> State:
+    """
+    This function searches params for the first rule that matches the state and the start_type.
+    None is treated as a wildcard. If no match is found, the function defaults.
+    """
+    for t_state, mon_state in params.get("states", [("Started", 0)]):
+        if _wildcard(t_state, service.state):
+            return State(mon_state)
+    return State(params.get("else", 2))
+
+
+def _wildcard(value, reference):
+    return value is None or value == reference
+
+
+def check_cisco_ucm_services(
+    item: str,
+    params: Mapping[str, Any],
+    section: Section,
+) -> Generator[Result, None, None]:
+    results = list(check_cisco_ucm_services_single(item, params, section))
+    if results:
+        yield from results
+    else:
+        yield Result(state=State(params.get("else", 2)), summary="service not found")
+
+
+def cluster_check_cisco_ucm_services(
+    item: str,
+    params: Mapping[str, Any],
+    section: Mapping[str, Section],
+) -> CheckResult:
+    # A service may appear more than once (due to clusters).
+    # First make a list of all matching entries with their
+    # states
+    found = []
+    for node, node_section in section.items():
+        results = list(check_cisco_ucm_services_single(item, params, node_section))
+        if results:
+            found.append((node, results[0]))
+
+    if not found:
+        yield Result(state=State(params.get("else", 2)), summary="service not found")
+        return
+
+    # We take the best found state (neccessary for clusters)
+    best_state = State.best(*(result.state for _node, result in found))
+    best_running_on, best_result = [(n, r) for n, r in found if r.state == best_state][-1]
+
+    yield best_result
+    if best_running_on and best_state != State.CRIT:
+        yield Result(state=best_state, summary="Running on: %s" % best_running_on)
+
+
 register.check_plugin(
-    name = "cisco_ucm_services",
-    service_name = "Services",
-    check_function = check_cisco_ucm_services,
-    discovery_function = discover_cisco_ucm_services,
+    name="cisco_ucm_services",
+    service_name="Service %s",
+    discovery_ruleset_type=register.RuleSetType.ALL,
+    discovery_ruleset_name="inventory_services_rules",
+    discovery_function=discovery_cisco_ucm_services,
+    discovery_default_parameters=CISCO_UCM_SERVICES_DISCOVERY_DEFAULT_PARAMETERS,
+    check_ruleset_name="cisco_ucm_services",
+    check_default_parameters=CISCO_UCM_SERVICES_CHECK_DEFAULT_PARAMETERS,
+    check_function=check_cisco_ucm_services,
+    cluster_check_function=cluster_check_cisco_ucm_services,
+)
+
+
+def discovery_cisco_ucm_services_summary(section: Section) -> DiscoveryResult:
+    if section:
+        yield Service()
+
+
+def check_cisco_ucm_services_summary(params: Mapping[str, Any], section: Section) -> CheckResult:
+    blacklist = params.get("ignored", [])
+    stoplist = []
+    num_blacklist = 0
+    num = 0
+
+    for service in section:
+        if service.state.lower() == "started":
+            num += 1
+        if service.state.lower() == "stopped":
+            if any(re.match(srv, service.name) for srv in blacklist):
+                num_blacklist += 1
+            else:
+                stoplist.append(service.name)
+
+    yield Result(
+        state=State.OK,
+        summary=f"Started services: {num}",
+        details=f"Started services: {num}\nServices found in total: {len(section)}",
+    )
+
+    yield Result(
+        state=State(params.get("state_if_stopped", 0)) if stoplist else State.OK,
+        summary=f"Stopped services: {len(stoplist)}",
+        details=("Stopped services: %s" % ', '.join(stoplist)) if stoplist else None,
+    )
+
+    if num_blacklist:
+        yield Result(state=State.OK, notice=f"Stopped but ignored: {num_blacklist}")
+
+
+register.check_plugin(
+    name="cisco_ucm_services_summary",
+    sections=["cisco_ucm_services"],
+    service_name="Service Summary",
+    discovery_function=discovery_cisco_ucm_services_summary,
+    check_function=check_cisco_ucm_services_summary,
+    check_default_parameters=CISCO_UCM_SERVICES_SUMMARY_DEFAULT_PARAMETERS,
+    check_ruleset_name="cisco_ucm_services_summary",
 )
---
+++ web/plugins/wato/cisco_ucm.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+
+import cmk.gui.watolib as watolib
+from cmk.gui.i18n import _
+from cmk.gui.plugins.wato import (
+    HostRulespec,
+    IndividualOrStoredPassword,
+    rulespec_registry,
+)
+from cmk.gui.plugins.wato.datasource_programs import RulespecGroupDatasourceProgramsApps
+from cmk.gui.plugins.wato import (
+    CheckParameterRulespecWithItem,
+    rulespec_registry,
+    RulespecGroupCheckParametersApplications,
+    RulespecGroupCheckParametersDiscovery,
+    HostRulespec,
+    UserIconOrAction,
+)
+from cmk.gui.valuespec import (
+    Alternative,
+    Dictionary,
+    DropdownChoice,
+    FixedValue,
+    Integer,
+    ListOf,
+    ListOfStrings,
+    MonitoringState,
+    TextAscii,
+    Tuple,
+)
+
+
+def _factory_default_special_agents_cisco_ucm():
+    # No default, do not use setting if no rule matches
+    return watolib.Rulespec.FACTORY_DEFAULT_UNUSED
+
+
+def _valuespec_special_agents_cisco_ucm():
+    return Dictionary(
+        title=_("Cisco UCM Services"),
+        help=_(
+            "This rule allows monitoring of services on Unified Communication Manager"
+            " via the Control Centere Services API (SOAP). "
+            "You can configure your connection settings here.",),
+        elements=[
+            ("user", TextAscii(
+                title=_("API User name"),
+                allow_empty=False,
+            )),
+            ("secret", IndividualOrStoredPassword(
+                title=_("API secret"),
+                allow_empty=False,
+            )),
+            ("tcp_port",
+             Integer(
+                 title=_("TCP Port number"),
+                 help=_("Port number for HTTPS connection"),
+                 default_value=8443,
+                 minvalue=1,
+                 maxvalue=65535,
+             )),
+            ("ssl",
+             Alternative(
+                 title=_("SSL certificate checking"),
+                 elements=[
+                     FixedValue(False, title=_("Deactivated"), totext=""),
+                     FixedValue(True, title=_("Use hostname"), totext=""),
+                     TextAscii(
+                         title=_("Use other hostname"),
+                         help=
+                         _("The IP of the other hostname needs to be the same IP as the host address"
+                          ))
+                 ],
+                 default_value=True)),
+            ("timeout",
+             Integer(
+                 title=_("Connect Timeout"),
+                 help=_(
+                     "The network timeout in seconds when communicating with CUCM or "
+                     "to the Check_MK Agent. The default is 60 seconds. Please note that this "
+                     "is not a total timeout but is applied to each individual network transation."
+                 ),
+                 default_value=60,
+                 minvalue=1,
+                 unit=_("seconds"),
+             )),
+        ],
+        optional_keys=[
+            "tcp_port",
+            "timeout",
+        ],
+    )
+
+
+rulespec_registry.register(
+    HostRulespec(
+        factory_default=_factory_default_special_agents_cisco_ucm(),
+        group=RulespecGroupDatasourceProgramsApps,
+        name="special_agents:cisco_ucm",
+        valuespec=_valuespec_special_agents_cisco_ucm,
+    ))
+
+
+def _valuespec_inventory_services_rules():
+    return Dictionary(
+        title=_("Cisco UCM service discovery"),
+        elements=[
+            ('cisco_ucm_services',
+             ListOfStrings(
+                 title=_("Services (Regular Expressions)"),
+                 help=_('Regular expressions matching the begining of the service name. '
+                        'If no name is given then this rule will match all services. The '
+                        'match is done on the <i>beginning</i> of the service name. It '
+                        'is done <i>case sensitive</i>. You can do a case insensitive match '
+                        'by prefixing the regular expression with <tt>(?i)</tt>. Example: '
+                        '<tt>(?i).*callmanager</tt> matches all services which contain <tt>CALLMANAGER</tt> '
+                        'or <tt>CallManager</tt> or <tt>callmanager</tt> or...'),
+                 orientation="horizontal",
+             )),
+            ('state',
+             DropdownChoice(
+                 choices=[
+                     ('started', _('Started')),
+                     ('stopped', _('Stopped')),
+                 ],
+                 title=_("Create check if service is in state"),
+             )),
+        ],
+        help=_(
+            'This rule can be used to configure the inventory of the Cisco UCM services check. '
+            'You can configure specific Cisco UCM services to be monitored by the check by '
+            'selecting them by name or by current state during the inventory.'),
+    )
+
+
+rulespec_registry.register(
+    HostRulespec(
+        group=RulespecGroupCheckParametersDiscovery,
+        match_type="all",
+        name="inventory_services_rules",
+        valuespec=_valuespec_inventory_services_rules,
+    ))
+
+
+def _item_spec_services():
+    return TextAscii(title=_("Name of the service"),
+                     allow_empty=False)
+
+
+def _parameter_valuespec_services():
+    return Dictionary(elements=[
+        ("additional_servicenames",
+         ListOfStrings(
+             title=_("Alternative names for the service"),
+             help=_("Here you can specify alternative names that the service might have. "
+                    "This helps when the exact spelling of the services can changed from "
+                    "one version to another."),
+         )),
+        ("states",
+         ListOf(
+             Tuple(orientation="horizontal",
+                   elements=[
+                       DropdownChoice(
+                           title=_("Expected state"),
+                           default_value="started",
+                           choices=[(None, _("ignore the state")), ("started", _("started")),
+                                    ("stopped", _("stopped"))],
+                       ),
+                       MonitoringState(title=_("Resulting state"),),
+                   ],
+                   default_value=("started", 0)),
+             title=_("Services states"),
+             help=_("You can specify a separate monitoring state for each possible "
+                    "combination of service state. If you do not use "
+                    "this parameter, then only started will be assumed to be OK."),
+         )),
+        ("else",
+         MonitoringState(
+                 title=_("State if no entry matches"),
+                 default_value=2,
+         )),
+    ],)
+
+
+rulespec_registry.register(
+    CheckParameterRulespecWithItem(
+        check_group_name="cisco_ucm_services",
+        group=RulespecGroupCheckParametersApplications,
+        item_spec=_item_spec_services,
+        match_type="dict",
+        parameter_valuespec=_parameter_valuespec_services,
+        title=lambda: _("Cisco UCM Services"),
+    ))
